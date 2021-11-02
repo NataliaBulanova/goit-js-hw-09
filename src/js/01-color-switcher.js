021d4008d5538f21083b70c1fa46389d35c2e818
NataliaBulanova <nbulanova91@gmail.com>
2021-11-02 12:31:23 +0200

    initial settings

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,16 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBTN = document.querySelector('[data-stop]');
+const COLORCHANGE_INTERVAL = 1000;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function changeColor(color) {
+  document.body.style.backgroundColor = color;
+}
 
+startBtn.addEventListener('click', () => {
+  let color = getRandomHexColor();
+  const timerId = setInterval(() => {
+    changeColor(color);
+  }, COLORCHANGE_INTERVAL);
+});
